57ce13cd6bb2c44bfd0d92aeba135ae4dc903ca0
Ankit Bhattarai <bhattaraiankit111@gmail.com>
2024-08-08 03:08:15 +0545

    Added EtherType and DnsRDataMInfo

--- Core/src/main/java/org/jcap/Core/Constants/NamedCodes/L2/EtherType.java
+++ Core/src/main/java/org/jcap/Core/Constants/NamedCodes/L2/EtherType.java
@@ -1,14 +1,82 @@
 package org.jcap.Core.Constants.NamedCodes.L2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jcap.Core.Constants.NamedCodes.NamedCode;
+import org.jcap.Core.Packets.Abstract_Layer_Packet.L3Packet;
+import org.jcap.Core.Packets.Network.IpV4Packet;
+import org.jcap.Core.Packets.Network.IpV6Packet;
 
 /**
  * Represents the EtherType in the ethernet frame....
+ * 
+ * /**
+ * Ether Type
+ *
+ * @see <a
+ *      href=
+ *      "http://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml#ieee-802-numbers-1">IANA
+ *      Registry</a>
  */
 public class EtherType extends NamedCode<Short, EtherType> {
 
-    public EtherType(Short value, String name) {
+    public static final int IEEE802_3_MAX_LENGTH = 1500;
+
+    Class<? extends L3Packet> clazz;
+    String desc;
+
+    public EtherType(Short value, String name, String desc, Class<? extends L3Packet> clazz) {
         super(value, name);
+        this.clazz = clazz;
+        this.desc = desc;
+    }
+
+    public static final EtherType IPv4 = new EtherType((short) 0x0800, "IPv4", "IPv4", IpV4Packet.class);
+    public static final EtherType ARP = new EtherType((short) 0x0806, "ARP", "ARP", null);
+    public static final EtherType WOL = new EtherType((short) 0x0842, "WOL", "Wake-on-LAN", null);
+    public static final EtherType RARP = new EtherType((short) 0x8035, "RARP", "Reverse ARP", null);
+    public static final EtherType IPv6 = new EtherType((short) 0x86DD, "IPv6", "IPv6", IpV6Packet.class);
+    public static final EtherType VLAN = new EtherType((short) 0x8100, "VLAN", "VLAN-tagged frame", null);
+    public static final EtherType PPPoE_Discovery = new EtherType((short) 0x8863, "PPPoE Discovery", "PPPoE Discovery",
+            null);
+    public static final EtherType PPPoE_Session = new EtherType((short) 0x8864, "PPPoE Session", "PPPoE Session", null);
+    public static final EtherType MPLS_UC = new EtherType((short) 0x8847, "MPLS Unicast", "MPLS Unicast", null);
+    public static final EtherType MPLS_MC = new EtherType((short) 0x8848, "MPLS Multicast", "MPLS Multicast", null);
+    public static final EtherType LLDP = new EtherType((short) 0x88CC, "LLDP", "Link Layer Discovery Protocol", null);
+    public static final EtherType SLOW = new EtherType((short) 0x8809, "SLOW", "Slow Protocols (IEEE 802.3)", null);
+    public static final EtherType EAPOL = new EtherType((short) 0x888E, "EAPOL",
+            "Extensible Authentication Protocol over LAN", null);
+    public static final EtherType QINQ = new EtherType((short) 0x88A8, "QinQ", "IEEE 802.1ad Service VLAN", null);
+
+    private static final Map<Short, EtherType> registry = new HashMap<>();
+
+    static {
+        registry.put(IPv4.getValue(), IPv4);
+        registry.put(ARP.getValue(), ARP);
+        registry.put(WOL.getValue(), WOL);
+        registry.put(RARP.getValue(), RARP);
+        registry.put(IPv6.getValue(), IPv6);
+        registry.put(VLAN.getValue(), VLAN);
+        registry.put(PPPoE_Discovery.getValue(), PPPoE_Discovery);
+        registry.put(PPPoE_Session.getValue(), PPPoE_Session);
+        registry.put(MPLS_UC.getValue(), MPLS_UC);
+        registry.put(MPLS_MC.getValue(), MPLS_MC);
+        registry.put(LLDP.getValue(), LLDP);
+        registry.put(SLOW.getValue(), SLOW);
+        registry.put(EAPOL.getValue(), EAPOL);
+        registry.put(QINQ.getValue(), QINQ);
+    }
+
+    public static EtherType instanceOfCode(Short code) {
+        if (registry.containsKey(code)) {
+            return registry.get(code);
+        } else if ((code & 0xFFFF) <= IEEE802_3_MAX_LENGTH) {
+            return new EtherType(code, "Length", "length", null);
+        } else {
+            return new EtherType(code, "unknown", "unknown", null);
+        }
+
     }
 
     @Override
@@ -16,4 +84,5 @@ public class EtherType extends NamedCode<Short, EtherType> {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
     }
+
 }
--- Core/src/main/java/org/jcap/Core/Packets/DNS/DnsRDataMInfo.java
+++ Core/src/main/java/org/jcap/Core/Packets/DNS/DnsRDataMInfo.java
@@ -1,7 +1,39 @@
 package org.jcap.Core.Packets.DNS;
 
-public class DnsRDataMInfo implements DnsRData
-/* implements DnsRData */ {
+import org.jcap.Core.Utils.ByteOperations;
+
+/**
+ * DNS MINFO RDATA
+ *
+ * <pre style="white-space: pre;">
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * /                    RMAILBX                    /
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * /                    EMAILBX                    /
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ *
+ * where:
+ *
+ * RMAILBX         A &lt;domain-name&gt; which specifies a mailbox which is
+ *                 responsible for the mailing list or mailbox.  If this
+ *                 domain name names the root, the owner of the MINFO RR is
+ *                 responsible for itself.  Note that many existing mailing
+ *                 lists use a mailbox X-request for the RMAILBX field of
+ *                 mailing list X, e.g., Msgroup-request for Msgroup.  This
+ *                 field provides a more general mechanism.
+ *
+ *
+ * EMAILBX         A &lt;domain-name&gt; which specifies a mailbox which is to
+ *                 receive error messages related to the mailing list or
+ *                 mailbox specified by the owner of the MINFO RR (similar
+ *                 to the ERRORS-TO: field which has been proposed).  If
+ *                 this domain name names the root, errors should be
+ *                 returned to the sender of the message.
+ * </pre>
+ *
+ * @see <a href="https://tools.ietf.org/html/rfc1035">RFC 1035</a>
+ */
+public class DnsRDataMInfo implements DnsRData {
 
     private final DnsDomainName RMAILBX;
     private final DnsDomainName EMAILBX;
@@ -10,6 +42,54 @@ public class DnsRDataMInfo implements DnsRData
         // TODO Validation is to be done
         this.EMAILBX = builder.EMAILBX;
         this.RMAILBX = builder.RMAILBX;
+    }
+
+    private DnsRDataMInfoBuilder Builder() {
+        return new DnsRDataMInfoBuilder();
+    }
+
+    private DnsRDataMInfoBuilder Builder(byte[] rawData, int offset, int len) {
+        ByteOperations.validate(rawData, offset, len);
+        return new DnsRDataMInfoBuilder(rawData, offset, len);
+    }
+
+    public DnsDomainName getRMailBx() {
+        return RMAILBX;
+    }
+
+    public DnsDomainName getEmailBx() {
+        return EMAILBX;
+    }
+
+    @Override
+    public int length() {
+        return RMAILBX.length() + EMAILBX.length();
+    }
+
+    @Override
+    public byte[] getRawData() {
+        byte[] rawData = new byte[length()];
+
+        byte[] rawRMX = RMAILBX.getRawData();
+        System.arraycopy(RMAILBX.getRawData(), 0, rawData, 0, rawRMX.length);
+
+        byte[] rawEMX = EMAILBX.getRawData();
+        System.arraycopy(rawEMX, 0, rawData, rawRMX.length, rawEMX.length);
+
+        return rawData;
+
+    }
+
+    @Override
+    public String toString(String indent) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+    @Override
+    public String toString(String indent, byte[] headerRawData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
     }
 
     public final static class DnsRDataMInfoBuilder {
@@ -21,8 +101,12 @@ public class DnsRDataMInfo implements DnsRData
         private DnsRDataMInfoBuilder() {
         }
 
-        private DnsRDataMInfoBuilder(byte rawData, int offset, int len) {
-            // TODO To be implemented......
+        private DnsRDataMInfoBuilder(byte[] rawData, int offset, int len) {
+            this.RMAILBX = DnsDomainName.Builder(rawData, offset, len).build();
+            if (len == RMAILBX.length())
+                throw new IllegalArgumentException("Data is too short to make the MINFO record");
+
+            this.EMAILBX = DnsDomainName.Builder(rawData, offset + RMAILBX.length(), len - RMAILBX.length()).build();
             this.sealed = true;
         }
 
@@ -47,29 +131,40 @@ public class DnsRDataMInfo implements DnsRData
         public DnsRDataMInfo build() {
             return new DnsRDataMInfo(this);
         }
+
     }
 
     @Override
-    public int length() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'length'");
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((RMAILBX == null) ? 0 : RMAILBX.hashCode());
+        result = prime * result + ((EMAILBX == null) ? 0 : EMAILBX.hashCode());
+        return result;
     }
 
     @Override
-    public byte[] getRawData() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRawData'");
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DnsRDataMInfo other = (DnsRDataMInfo) obj;
+        if (RMAILBX == null) {
+            if (other.RMAILBX != null)
+                return false;
+        } else if (!RMAILBX.equals(other.RMAILBX))
+            return false;
+        if (EMAILBX == null) {
+            if (other.EMAILBX != null)
+                return false;
+        } else if (!EMAILBX.equals(other.EMAILBX))
+            return false;
+        return true;
     }
 
-    @Override
-    public String toString(String indent) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
-    }
+    
 
-    @Override
-    public String toString(String indent, byte[] headerRawData) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
-    }
 }
